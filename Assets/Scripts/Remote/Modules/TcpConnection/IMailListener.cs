f93962d1288a487546c2429b3f16b55d08ccb3dc
YangDR <ydrml@hotmail.com>
2019-01-09 10:38:16 +0800

    更新库
    Signed-off-by: YangDR <ydrml@hotmail.com>

--- Assets/Scripts/Remote/Modules/TcpConnection/IMailListener.cs
+++
@@ -1,7 +0,0 @@
-using MechDancer.Framework.Dependency;
-
-namespace MechDancer.Framework.Net.Modules.TcpConnection {
-	public interface IMailListener : IComponent {
-		void Process(string sender, byte[] payload);
-	}
-}
